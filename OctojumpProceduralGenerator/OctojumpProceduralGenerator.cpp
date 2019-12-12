43f991852f5f15aaf8e4a3db30a60187e23f1f65
SpectraIl <35639848+SpectraIl@users.noreply.github.com>
2019-12-12 12:59:45 +0100

    Replace main.cpp

--- OctojumpProceduralGenerator/OctojumpProceduralGenerator.cpp
+++ OctojumpProceduralGenerator/OctojumpProceduralGenerator.cpp
@@ -7,47 +7,77 @@
 
 using namespace std;
 
+// 4 1 // 8 2 // 12 3 // 16 4 // 20 5 // 24 6 // 28 7 // 32 8 // 36 9 // 40 10 // 44 11 // 48 12 // 52 13 // 56 14 // 60 15 // 64 16 // 68 17 // 72 18 // 76 19 // 80 20
+// 84 21 // 88 22 // 92 23 // 96 24 // 100 25 // 104 26 // 108 27 // 112 28 // 116 29 // 120 30 // 124 31  // 128 32 // 132 33 // 136 34 // 140 35 // 144 36 // 148 37 // 152 38 // 156 39 // 160 40
+
 int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        debug();
-    } else {
-        if (argc == 4 || argc == 5) {
-            int *iSize = nullptr;
-            int *iRandomFillPercent = nullptr;
-            MapGenerator *mg = nullptr;
-            wchar_t *buffer;
-            int bufferSize;
+	if (argc == 1) {
+		debug();
+	}
+	else {
+		if (argc == 6 || argc == 7) {
+			int *iSize = nullptr;
+			int *iRandomFillPercent = nullptr;
+			int *iBuildingMaxHeight = nullptr;
+			int* iSpaceBetweenRoads = nullptr;
 
-            stringToInt(string(argv[1]), &iSize);
-            if (iSize == nullptr) {
-                return 1;
-            }
-            stringToInt(string(argv[2]), &iRandomFillPercent);
-            if (iRandomFillPercent == nullptr) {
-                return 1;
-            }
+			MapGenerator *mg = nullptr;
+			wchar_t *buffer;
+			int bufferSize;
 
-            if (argc == 4) {
-                mg = new MapGenerator(*iSize, *iRandomFillPercent);
+			//old (argc 5)
+			//0
+			//1 iSize
+			//2 iRandomFillPercent
+			//3 seed -- opt
+			//4 levelToSave
+			//new (argc 7)
+			//0
+			//1 iSize
+			//2 irandomFillPercent
+			//3 iBuildingMaxHeight
+			//4 iSpaceBetweenRoads
+			//5 levelToSave
+			//6 seed -- opt
 
-            } else {
-                mg = new MapGenerator(*iSize, *iRandomFillPercent, string(argv[3]));
-            }
+			stringToInt(string(argv[1]), &iSize);
+			if (iSize == nullptr) {
+				return 1;
+			}
+			stringToInt(string(argv[2]), &iRandomFillPercent);
+			if (iRandomFillPercent == nullptr) {
+				return 1;
+			}
+			stringToInt(string(argv[3]), &iBuildingMaxHeight);
+			if (iBuildingMaxHeight == nullptr) {
+				return 1;
+			}
+			stringToInt(string(argv[4]), &iSpaceBetweenRoads);
+			if (iSpaceBetweenRoads == nullptr) {
+				return 1;
+			}
 
-            mg->generateMap();
+			if (argc == 7) {
+				mg = new MapGenerator(*iSize, *iRandomFillPercent, string(argv[6]));
 
-            bufferSize = MultiByteToWideChar(CP_ACP, 0, argv[argc - 1], -1, nullptr, 0);
-            buffer = new wchar_t[bufferSize];
-            MultiByteToWideChar(CP_ACP, 0, argv[argc - 1], -1, (LPWSTR) buffer, bufferSize);
-            saveFile(buffer, mg->getMap(), *iSize);
+			}
+			else {
+				mg = new MapGenerator(*iSize, *iRandomFillPercent);
+			}
 
-            delete mg;
-        } else {
-            cerr << "Error! Wrong number of arguments provided!" << endl;
-            return -1;
-        }
-    }
-    return 0;
+			mg->generateMap();
+
+			bufferSize = MultiByteToWideChar(CP_ACP, 0, argv[5], -1, nullptr, 0);
+			buffer = new wchar_t[bufferSize];
+			MultiByteToWideChar(CP_ACP, 0, argv[5], -1, (LPWSTR)buffer, bufferSize);
+			saveFile(buffer, mg->getMap(), *iSize);
+
+			delete mg;
+		}
+		else {
+			cerr << "Error: Wrong number of arguments provided (You have provided " << argc << " arguments)" << endl;
+			return -1;
+		}
+	}
+	return 0;
 }
-
-
